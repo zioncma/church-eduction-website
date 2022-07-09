7d0a5171486f4f86b8ba1ef3cc5475e0892d0bd4
Asher Yang <30133546+CeruleanW@users.noreply.github.com>
2022-07-09 22:15:38 +0000

    change width

--- src/components/features/Podcast/index.tsx
+++ src/components/features/Podcast/index.tsx
@@ -3,6 +3,7 @@ import styled from 'styled-components';
 const StyledIframe = styled.iframe`
   border: none; 
   min-width: min(100%, 430px);
+  max-width: 100%;
 `;
 
 export function Podcast() {
@@ -12,8 +13,7 @@ export function Podcast() {
     <StyledIframe
       title='Zion Alliance Church Christian Education Podcast'
       allowtransparency={true}
-      height='315'
-      width='100%'
+      // height='315'
       scrolling='no'
       data-name='pb-iframe-player'
       src='https://www.podbean.com/player-v2/?i=dn859-dfb3c4-pbblog-playlist&share=1&download=1&rtl=0&fonts=Arial&skin=1&font-color=auto&logo_link=episode_page&order=episodic&limit=10&filter=all&ss=a713390a017602015775e868a2cf26b0&btn-skin=7&size=315'
