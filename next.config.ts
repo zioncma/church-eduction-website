7ea8862670681db4d386046a607f3e7d1738cceb
Asher <ayang@safetrack.ai>
2025-05-24 21:51:55 -0400

    enable a static export

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,8 @@
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  // output: 'export', // Outputs a Single-Page Application (SPA)
-  distDir: 'build', // Changes the build output directory to `build`
+  output: 'export', // Outputs a Single-Page Application (SPA)
+  // distDir: 'build', // Changes the build output directory to `build`
   reactStrictMode: false, // Disable React Strict Mode (Temporary Fix for Material-UI (@mui/core))
 };
 
